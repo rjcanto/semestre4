a3b29dd0fe14912e735f0b612d794b816ff34ac7
nuno.cancelo <nuno.cancelo@4dcf1070-26bf-11df-a94c-d58e26249b9a>
2010-03-09 16:55:32 +0000

    PSC: Trabalho Final

---
+++ PSC/SERIES/Serie01/src/exercicio2.h
@@ -0,0 +1,15 @@
+/*
+Turma: 	LI31N
+Nome: 	Nuno Cancelo
+Numero: 31401
+*/
+#ifndef _SERIE01_EX02
+#define _SERIE01_EX02
+
+#include "serie01.h"
+#define STR_MAX_SIZE 255
+#define MAX_SIZE 510
+
+char * xstrcat(char * str1, const char * str2);
+
+#endif
---
+++ PSC/SERIES/Serie01/src/serie01.h
@@ -0,0 +1,16 @@
+/*
+Turma: 	LI31N
+Nome: 	Nuno Cancelo
+Numero: 31401
+*/
+#ifndef _SERIE01
+#define _SERIE01
+/*
+ * Constantes definidas
+ * */
+#define SUCCESS 0
+#define UNSUCCESS 1
+#define TRUE 1
+#define FALSE 0
+
+#endif
