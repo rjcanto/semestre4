fd8f5ac562e1809b1a1ee51383880bca0e86d01e
nuno.cancelo <nuno.cancelo@4dcf1070-26bf-11df-a94c-d58e26249b9a>
2010-06-09 22:00:24 +0000

    ++psc

--- trunk/PSC/SERIES/Serie04/src/CDB_Builder.c
+++ trunk/PSC/SERIES/Serie04/src/CDB_Builder.c
@@ -1,14 +1,5 @@
 #include "CDB_Builder.h"
 
-byte* CDB_field_equalize(char**  field, char * idx){
-	if (*idx != 0){
-		*field = strndup(idx+1,*idx);
-		idx = idx + 1 + *idx;
-	}else{
-		*field=NULL;
-		++idx;
-	}
-	return idx;
-}
+
 
 
--- trunk/PSC/SERIES/Serie04/src/Command.h
+++ trunk/PSC/SERIES/Serie04/src/Command.h
@@ -3,9 +3,16 @@
 #include "Common.h"
 #include "Teacher.h"
 #include "UniCurr.h"
-#include "CDB_Builder.h"
+
+#include <cdb.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <cdb.h>
 unsigned short twoByte2UnsignedShort(char** twobyte);
+byte*  CDB_field_equalize(char** field, char* value);
 void unsignedShort2TwoBytes(char* dest, const unsigned short ush);
 void Command_dblist(struct cdb* cdb, char* key, unsigned klen, void (*parser)(char*));
 void Command_dbReader(char* filename,char * key, void (*fx)(struct cdb* , char* , unsigned, void (*parser)(char*) ), void (*parser)(char*) );
