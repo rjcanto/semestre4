dd76b9be888ce829372c8ec3041a5167ba698fed
ng.sousa@gmail.com <ng.sousa@gmail.com@4dcf1070-26bf-11df-a94c-d58e26249b9a>
2010-08-11 23:30:57 +0000

    model/Board.java com select actualizado

--- POO/TRABALHOS/Trabalho3/src/SameGame/Game/model/Board.java
+++ POO/TRABALHOS/Trabalho3/src/SameGame/Game/model/Board.java
@@ -10,7 +10,7 @@ import java.util.Random;
  * @author Nuno
  */
 
-public class Board implements Board_I{
+public class Board implements Board_I, GameModelVars_I{
     private Block[][] grid;
     private int rows;
     private int columns;
@@ -75,9 +75,24 @@ public class Board implements Board_I{
         remainingBlocks=0;
     }
 
+    /*
+     * verifica se o board tem alguma coluna vazia
+     * @return retorna indice da primeira coluna vazia ou numero de colunas se nenhuma
+     */
+    public int checkEmptyColumn(){
+        for (int i=0;i<columns;++i)
+            if (grid[rows-1][i]==null)
+                return i;
+        return -1;
+    }
 
     /*
-     * Métodos para manipulação do Board
+     * Métodos para manipulação da Grelha
+     */
+    /*
+     * Roda o tabuleiro de jogo
+     * @param right se true roda para a direita, se false para a esquerda
+     * @return boolean true se foi possível rodar, false se falhou
      */
     public boolean rotate(boolean right) {
         if (rows!=columns || remainingBlocks==0)
@@ -139,6 +154,11 @@ public class Board implements Board_I{
             }
         }
     }
+    public void shitPartiallyLeft(int emptyColumn){
+        
+
+
+    }
 
     /*
      * Inicialização do Board.
@@ -160,181 +180,124 @@ public class Board implements Board_I{
         }
         return true;
     }
-    
+    public void addColumnToEnd(String[] blockNames){
+
+
+    }
     /*
      *Métodos para selecção de blocos
      */
     public boolean select(int r, int c) {
-        
+        if (!isValid(r, c)) return false;
         boolean[][] rule = getBlock(r,c).getSelectionRule();
         boolean selected = select(r,c,rule);
-        if (selected)
-            selectAndSave(r,c);
-        
+        if (selected){
+            grid[r][c].select();
+            selectedBlocks.add(new Coordinates(r,c));
+        }
         return (selectedBlocks.size()>0)? true:false;
-        
     }
 
+    /*
+     * Método para seleccionar recursivamente blocos de acordo com as rules
+     * passadas e até à distância radius.
+     * No final do mesmo os blocos terao a propriedade selected a true e foram
+     * adicionados à lista SelectedBlocks
+     * 
+     * @param r linha a que pertence o bloco a seleccionar
+     * @param c coluna a que pertence o bloco a seleccionar
+     * @param rule variável com as regras de selecção a utilizar
+     * @param radius distância até à qual deve ser efectuada a selecção
+     * @return retorna true se foi efectuada alguma selecção ou false no caso
+     *          contrário
+     */
     private boolean select(int r, int c, boolean[][] rule){
         Block b = this.removeBlock(r, c);
-        /*
-         * Verifica canto superior esquerdo
-         */
-        if (rule[0][0] && c!=0 && r!=0)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r-1][c-1])==0){
-                    this.select(r-1,c-1,rule);
-                    selectAndSave(r-1,c-1);
-                }
-            }else{
-                if (grid[r-1][c-1]!=null){
-                    this.select(r-1,c-1,rule);
-                    selectAndSave(r-1,c-1);
-                }
-            }
 
-        /*
-         * Verifica cima
-         */
-        if (rule[0][1] && r!=0)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r-1][c])==0){
-                    this.select(r-1,c,rule);
-                    selectAndSave(r-1, c);
-                }
-            }else{
-                if (grid[r-1][c]!=null){
-                    this.select(r-1,c,rule);
-                    selectAndSave(r-1, c);
-                }
-            }
-        /*
-         * Verifica canto superior direito
-         */
-        if (rule[0][2] && c!=columns-1 && r!=0)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r-1][c+1])==0){
-                    this.select(r-1,c+1,rule);
-                    selectAndSave(r-1,c+1);
-                }
-            }else{
-                if (grid[r-1][c+1]!=null){
-                    this.select(r-1,c+1,rule);
-                    selectAndSave(r-1,c+1);
-                }
-            }
-        /*
-         * Verifica o lado esquerdo
-         */
-        if (rule[1][0] && c!=0)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r][c-1])==0){
-                    this.select(r,c-1,rule);
-                    selectAndSave(r,c-1);
-                }
-            }else{
-                if (grid[r][c-1]!=null){
-                    this.select(r,c-1,rule);
-                    selectAndSave(r,c-1);
-                }
+        for (int i=0; i<rule.length;++i)
+            for (int j=0; j<rule[0].length;++j){
+                if (rule[i][j])
+                    checkBlock(r+ROW_OFFSET[i][j] , c+(COLUMN_OFFSET[i][j]), rule, b);
+        }
+//        for (int i=0; i<rule.length;++i)
+//            for (int j=0; j<rule[0].length;++j){
+//                if (rule[i][j])
+//                   checkBlock(r+OFFSET[i][j].getRow() , c+OFFSET[i][j].getColumn(), rule, b);
+//        }
 
-            }
-        /*
-         * Verifica o lado direito
-         */
-        if (rule[1][2] && c!=columns-1)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r][c+1])==0){
-                    this.select(r-1,c+1,rule);
-                    selectAndSave(r-1,c+1);
-                }
-            }else{
-                if (grid[r-1][c+1]!=null){
-                    this.select(r-1,c+1,rule);
-                    selectAndSave(r-1,c+1);
-                }
-            }
-        /*
-         * Verifica o canto inferior esquerdo
-         */
-        if (rule[2][0] && r!=rows-1 && c!=0)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r+1][c-1])==0){
-                    this.select(r+1,c-1,rule);
-                    selectAndSave(r+1,c-1);
-                }
-            }else{
-                if (grid[r+1][c-1]!=null){
-                    this.select(r+1,c-1,rule);
-                    selectAndSave(r+1,c-1);
-                }
-            }
-        /*
-         * Verifica baixo
-         */
-        if (rule[2][1] && r!=rows-1)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r+1][c])==0){
-                    this.select(r+1,c,rule);
-                    selectAndSave(r+1,c);
-                }
-            }else{
-                if (grid[r+1][c]!=null){
-                    this.select(r+1,c,rule);
-                    selectAndSave(r+1,c);
-                }
-            }
-        /*
-         * Verifica o canto inferior direito
-         */
-        if (rule[2][2] && r!= rows-1 && c!=columns-1)
-            if (rule[1][1]){
-                if (b.compareTo(grid[r+1][c+1])==0){
-                    this.select(r+1,c+1,rule);
-                    selectAndSave(r+1,c+1);
-                }
-            }else{
-                if (grid[r+1][c+1]!=null){
-                    this.select(r+1,c+1,rule);
-                    selectAndSave(r+1,c+1);
-                }
-            }
+//
+//        /*Verifica canto superior esquerdo*/
+//        if (rule[0][0] && c!=0 && r!=0)
+//            checkBlock(r+TOP_LEFT[0],c+TOP_LEFT[1],rule,b);
+//        /*Verifica cima*/
+//        if (rule[0][1] && r!=0)
+//           checkBlock(r+TOP[0],c+TOP[1],rule,b);
+//        /*Verifica canto superior direito*/
+//        if (rule[0][2] && c!=columns-1 && r!=0)
+//            checkBlock(r+TOP_RIGHT[0],c+TOP_RIGHT[1],rule,b);
+//        /*Verifica o lado esquerdo*/
+//        if (rule[1][0] && c!=0)
+//           checkBlock(r+LEFT[0],c+LEFT[1],rule,b);
+//        /*Verifica o lado direito*/
+//        if (rule[1][2] && c!=columns-1)
+//            checkBlock(r+RIGHT[0],c+RIGHT[1],rule,b);
+//        /*Verifica o canto inferior esquerdo*/
+//        if (rule[2][0] && r!=rows-1 && c!=0)
+//            checkBlock(r+BOTTOM_LEFT[0],c+BOTTOM_LEFT[1],rule,b);
+//        /*Verifica baixo*/
+//        if (rule[2][1] && r!=rows-1)
+//            checkBlock(r+BOTTOM[0],c+BOTTOM[1],rule,b);
+//        /*Verifica o canto inferior direito*/
+//        if (rule[2][2] && r!= rows-1 && c!=columns-1)
+//            checkBlock(r+BOTTOM_RIGHT[0],c+BOTTOM_RIGHT[1],rule,b);
 
         addBlock(b, r, c);
         return (selectedBlocks.size()>0)? true:false;
     }
 
-    private void selectAndSave(int r, int c){
-        grid[r][c].select();
-        selectedBlocks.add(new Coordinates(r,c));
+    private void checkBlock(int r, int c, boolean[][] rule, Block b){
+        if (!isValid(r, c)) return;
+        if (rule[1][1]){
+            if (grid[r][c]!=null && b.compareTo(grid[r][c])==0){
+                this.select(r,c,rule);
+                grid[r][c].select();
+                selectedBlocks.add(new Coordinates(r,c));
+            }
+        }else if (grid[r][c]!=null){
+                this.select(r,c,rule);
+                grid[r][c].select();
+                selectedBlocks.add(new Coordinates(r,c));
+        }
     }
 
-//    public Iterator<Block> getSelectedBlocks() {
-//        throw new UnsupportedOperationException("Not supported yet.");
-//    }
-
     public void unselect(){
+        if (selectedBlocks.size()==0)
+            return;
         Iterator<Coordinates> it = selectedBlocks.iterator();
         Coordinates c;
         while (it.hasNext()){
             c = it.next();
             grid[c.getRow()][c.getColumn()].unselect();
-            selectedBlocks.remove();
+            it.remove();
         }
     }
     public void unselect(int r, int c){
-        Coordinates coord = new Coordinates(r,c);
         if (!grid[r][c].isSelected())
             return;
+        this.unselect();
+    }
+    public int removeSelected(){
+        int result = selectedBlocks.size();
+        if (result==0)
+            return 0;
         Iterator<Coordinates> it = selectedBlocks.iterator();
-        Coordinates aux;
+        Coordinates c;
         while (it.hasNext()){
-            aux = it.next();
-            if (aux.equals(coord)){
-                grid[coord.getRow()][coord.getColumn()].unselect();
-                selectedBlocks.remove();
-            }
+            c = it.next();
+            this.removeBlock(c.getRow(),c.getColumn());
+            it.remove();
         }
+        return result;
     }
 
     public boolean isSelected(int r, int c) {
@@ -346,7 +309,7 @@ public class Board implements Board_I{
         String res="";
         for (int i=0; i<rows;++i){
             for (int j=0; j<columns;++j)
-                res+=grid[i][j]==null? "-:- | ":grid[i][j].getType()+ ":" +grid[i][j].isSelected() + " | ";
+                res+=grid[i][j]==null? "-:- | ":grid[i][j].getType()+ ":"+grid[i][j].getColor().getRGB()+ ":" +grid[i][j].isSelected() + " | ";
             res+='\n';
         }
         return res;
